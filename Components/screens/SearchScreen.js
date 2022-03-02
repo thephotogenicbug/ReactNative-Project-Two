d7fa66012f21502ef26d3d01c8b906f69fba742a
thephotogenicbug <navinkumarachari@gmail.com>
2022-03-02 15:11:53 +0530

    02-03-2022 : Handle Error

--- Components/screens/SearchScreen.js
+++ Components/screens/SearchScreen.js
@@ -5,16 +5,21 @@ import yelp from "../../api/yelp";
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        searchTerm,
-        location: "san jose",
-      },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
   };
 
   return (
@@ -24,7 +29,7 @@ const SearchScreen = () => {
         onChange={setSearchTerm}
         onSubmit={searchApi}
       />
-      <Text>SearchScreen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found: {results.length} results</Text>
     </View>
   );
